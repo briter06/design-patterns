cec179ab3380bce3dc2b0b85c3ad4f5fd03e44d7
Briter Gonzalez <btg.developers@gmail.com>
2023-06-01 08:59:24 -0500

    feat(patterns): chalk added for colors

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import chalk from "chalk";
 import pascalcase from "pascalcase";
 import { singletonIndex } from "./patterns/creational/singleton";
 
@@ -7,7 +8,7 @@ const patterns: {[key: string]: () => void} = {
 
 const pattern: string = process.argv[2]
 if(patterns[pattern]){
-    console.log(`${pascalcase(pattern)} pattern:`)
+    console.log(chalk.yellow(`${pascalcase(pattern)} pattern:`))
     patterns[pattern]()
 }else{
     throw new Error('Invalid pattern')
--- src/patterns/creational/singleton/index.ts
+++ src/patterns/creational/singleton/index.ts
@@ -1,18 +1,19 @@
 import { SingletonObject } from "./SingletonObject";
+import chalk from "chalk";
 
 export const singletonIndex = () => {
     // Instance 1
-    console.log("Instance 1:")
+    console.log(chalk.green("Instance 1:"))
     const intance1 = SingletonObject.getInstance()
-    console.log('- Add "Value 1"')
+    console.log(`${chalk.blue('- Add')} "Value 1"`)
     intance1.addValue("Value 1")
-    console.log(`- Result: ${JSON.stringify(intance1.getValues())}`)
+    console.log(`${chalk.blue('- Result:')} ${JSON.stringify(intance1.getValues())}`)
 
     console.log("--------------")
     // Instance 2
-    console.log("Instance 2:")
+    console.log(chalk.green("Instance 2:"))
     const intance2 = SingletonObject.getInstance()
-    console.log('- Add "Value 2"')
+    console.log(`${chalk.blue('- Add')} "Value 2"`)
     intance2.addValue("Value 2")
-    console.log(`- Result: ${JSON.stringify(intance1.getValues())}`)
+    console.log(`${chalk.blue('- Result:')} ${JSON.stringify(intance1.getValues())}`)
 }
