f54b3e31489020e2073b7cd3c5ddb36e08033d61
Briter Gonzalez <btg.developers@gmail.com>
2023-06-01 08:55:05 -0500

    feat(pattern): improve singleto example

--- src/patterns/creational/singleton/index.ts
+++ src/patterns/creational/singleton/index.ts
@@ -2,7 +2,17 @@ import { SingletonObject } from "./SingletonObject";
 
 export const singletonIndex = () => {
     // Instance 1
+    console.log("Instance 1:")
     const intance1 = SingletonObject.getInstance()
+    console.log('- Add "Value 1"')
     intance1.addValue("Value 1")
-    console.log(intance1.getValues())
+    console.log(`- Result: ${JSON.stringify(intance1.getValues())}`)
+
+    console.log("--------------")
+    // Instance 2
+    console.log("Instance 2:")
+    const intance2 = SingletonObject.getInstance()
+    console.log('- Add "Value 2"')
+    intance2.addValue("Value 2")
+    console.log(`- Result: ${JSON.stringify(intance1.getValues())}`)
 }
